0a5c36346964cfb5a9194170c8c390f5873e9b2b
Serhii T <88stysh88@gmail.com>
2021-06-19 14:01:33 -0500

    Pet class added Pet controller added

---
+++ API/Controllers/PetController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VetClinic.Shared;
+using Microsoft.Extensions.Logging;
+
+namespace VetClinic.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetController : Controller
+    {
+        private readonly ILogger<PetController> logger;
+
+        public PetController(ILogger<PetController> logger)
+        {
+            this.logger = logger;
+        }            
+
+        [HttpGet]
+        public IEnumerable<Pet> Get()
+        {
+            return new List<Pet> 
+            { 
+                new Pet { Name = "Tom", Kind = "Cat", Age = 3 },
+                new Pet { Name = "Jery", Kind = "Mouse", Age = 2 } 
+            };
+        }
+    }
+}
